df277601463998c39f02b22273cdb8a7ec4cdfac
mhibio <mhibio@gmail.com>
2025-11-28 07:26:37 +0900

    [backend, chore] change workdir setting

--- backend/run.sh
+++ backend/run.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
 python3 /app/pre_start.py
-
-# CMD ["uvicorn", "SSSP.api.app:apimain", "--host", "0.0.0.0", "--port", "443", "--reload"]
-uvicorn SSSP.api.app:apimain --host 0.0.0.0 --port 443
+uvicorn SSSP.api.app:apimain --host 0.0.0.0 --port 443 --reload
 
